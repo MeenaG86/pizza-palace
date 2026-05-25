fc0cf0625715e208a70e958478ff0998a272eac1
MeenaG86 <meeash2021@gmail.com>
2026-05-25 22:46:40 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,14 @@ const app = express();
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://pizza-palace-ebon.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use(express.json());
 
