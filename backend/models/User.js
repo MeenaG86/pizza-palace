61c94a4822c77633d547d81066b3ab8048b8cad7
MeenaG86 <meeash2021@gmail.com>
2026-05-28 21:32:01 +0530

    user updated

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,17 +5,26 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    trim: true,
   },
 
   email: {
     type: String,
     required: true,
     unique: true,
+    lowercase: true,
   },
 
   password: {
     type: String,
     required: true,
+    minlength: 6,
+  },
+
+  role: {
+    type: String,
+    enum: ["customer", "admin"],
+    default: "customer",
   },
 
 }, {
